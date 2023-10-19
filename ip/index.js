5380580ad3fd42de25422ebcd92eb289476ecd62
Beren Ates <berenatess9918@gmail.com>
2023-10-20 00:22:49 +0300

    Week5-Day4

--- ip/index.js
+++ ip/index.js
@@ -1,26 +1,24 @@
 //axios import buraya gelecek
-
+import axios from "axios";
 var benimIP;
-
 
 // ------------ değiştirmeyin --------------
 // licensed to Ergineer 2022
 require("babel-core/register");
 require("babel-polyfill");
-async function ipAdresimiAl(){
-	await axios({
-		method: 'get',
-		url: 'https://apis.ergineer.com/ipadresim',
-	})
-	.then(function (response) {
-		return response.data
-	})
-	.then(function (a) {
-		benimIP=a
-	});
-}				
+async function ipAdresimiAl() {
+  await axios({
+    method: "get",
+    url: "https://apis.ergineer.com/ipadresim",
+  })
+    .then(function (response) {
+      return response.data;
+    })
+    .then(function (a) {
+      benimIP = a;
+    });
+}
 // ------------ değiştirmeyin --------------
-
 
 /*
 	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
@@ -67,6 +65,86 @@ async function ipAdresimiAl(){
 	Örnek dinamik URL kullanımı: var url = "https://apis.ergineer.com/ipgeoapi/"+benimIP; 
 */
 
-
-
 //kodlar buraya gelecek
+
+const locationData = {
+  as: "AS8386 Vodafone Net Iletisim Hizmetler AS",
+  boylam: 29.0659,
+  bölge: "16",
+  bölgeAdı: "Bursa Province",
+  durum: "OK",
+  enlem: 40.1621,
+  isp: "Vodafone Net DSL Bursa6",
+  kıta: "Asia",
+  organizasyon: "Vodafone NET Iletisim Hizmetleri Anonim Sirketi",
+  parabirimi: "TRY",
+  saatdilimi: "Europe/Istanbul",
+  sorgu: "31.206.203.45",
+  zip: "16360",
+  ülke: "Turkey",
+  ülkeKodu: "TR",
+  ülkebayrağı: "https://apis.ergineer.com/ulkebayraklari/TR",
+  şehir: "Bursa",
+};
+
+const cardContainer = (data) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const image = document.createElement("img");
+  image.src = "https://flagcdn.com/w640/tr.png";
+  card.appendChild(image);
+
+  const cardClass = document.createElement("div");
+  cardClass.classList.add("card-info");
+  card.appendChild(cardClass);
+
+  const h3 = document.createElement("h3");
+  h3.classList.add("ip");
+  h3.textContent = data.sorgu;
+  cardClass.appendChild(h3);
+
+  const p = document.createElement("p");
+  p.classList.add = "ulke";
+  p.textContent = data.ülke + " " + "(" + data.ülkeKodu + ")";
+  cardClass.appendChild(p);
+
+  const enlemBoylam = document.createElement("p");
+  enlemBoylam.textContent = `Enlem: ${data.enlem} Boylam: ${data.boylam}`;
+  cardClass.appendChild(enlemBoylam);
+
+  const sehir = document.createElement("p");
+  sehir.textContent = `Şehir: ${data.şehir}`;
+  cardClass.appendChild(sehir);
+
+  const saat = document.createElement("p");
+  saat.textContent = `Saat Dilimi: ${data.saatdilimi}`;
+  cardClass.appendChild(saat);
+
+  const parabirimi = document.createElement("p");
+  parabirimi.textContent = `Para birimi: ${data.parabirimi}`;
+  cardClass.appendChild(parabirimi);
+
+  const isp = document.createElement("p");
+  isp.textContent = `ISP: ${data.isp}`;
+  cardClass.appendChild(isp);
+
+  return card;
+};
+
+const cards = document.querySelector(".cards");
+
+const createIP = async () => {
+  await ipAdresimiAl();
+
+  axios
+    .get("https://apis.ergineer.com/ipgeoapi/" + benimIP)
+    .then((res) => {
+      console.log("axios", res.data);
+      return res.data;
+    })
+    .then((ip) => {
+      cards.appendChild(cardContainer(ip));
+    });
+};
+createIP();
